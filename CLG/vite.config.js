507f62375832492c5b992b10de265154fe1d71b5
N1077433 <joerkerry@gmail.com>
2026-02-03 02:09:23 +0000

    refactored CLG package configuration and Vite setup

--- CLG/vite.config.js
+++ CLG/vite.config.js
@@ -3,8 +3,23 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/CoverLetterGenerator/',
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'query-vendor': ['@tanstack/react-query'],
+          'pdf-vendor': ['jspdf', 'html2canvas'],
+          'docx-vendor': ['docx', 'file-saver']
+        }
+      }
+    }
+  },
   server: {
+    port: 3000,
     allowedHosts: ['obvolutive-impecuniously-rena.ngrok-free.dev']
   }
 })
